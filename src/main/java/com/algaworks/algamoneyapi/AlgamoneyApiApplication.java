39f39149a5b534a1b97622e78279540604529ef4
Bruno Souza <brunoprogramadorjava@gmail.com>
2019-06-21 02:22:26 -0300

    [3.3] Migração de dados com flyway v1

--- src/main/java/com/algaworks/algamoneyapi/AlgamoneyApiApplication.java
+++ src/main/java/com/algaworks/algamoneyapi/AlgamoneyApiApplication.java
@@ -1,4 +1,4 @@
-package com.algaworks.algamoney.api;
+package com.algaworks.algamoneyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
